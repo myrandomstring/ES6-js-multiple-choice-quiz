48071c3e37bac02314de6de011e983fc1c140ad7
myrandomstring <adamjacksonuk@gmail.com>
2024-02-20 12:01:11 +0000

    -

--- private/extDOMImplementation.php
+++ private/extDOMImplementation.php
@@ -2,19 +2,19 @@
 require_once("extDOMDocument.php");
 class extDOMImplementation extends DOMImplementation
 {   
-	public function __construct()
-	{
-		//return $this->createDocument();
-	}
+    public function __construct()
+    {
+        //return $this->createDocument();
+    }
 
-	public function createDocument($namespaceURI=null, $qualifiedName='html', DOMDocumentType $docType=null)
+    public function createDocument($namespaceURI=null, $qualifiedName='html', DOMDocumentType $docType=null)
     {    
-		$doc = new extDOMDocument();
+        $doc = new extDOMDocument();
         $doc->appendChild(parent::createDocumentType('html','',''));
-		$doc->registerNodeClass('DOMElement','extDOMElement');
-		//$doc->validate();
-		
-		return $doc;
-	}
+        $doc->registerNodeClass('DOMElement','extDOMElement');
+        //$doc->validate();
+        
+        return $doc;
+    }
 }
 ?>
